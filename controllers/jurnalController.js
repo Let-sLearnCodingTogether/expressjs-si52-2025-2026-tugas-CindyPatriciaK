6d815876386f049ac2e970cf42faaae1193f221f
CindyPatriciaK <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2025-10-17 08:05:43 +0700

    .

--- controllers/jurnalController.js
+++ controllers/jurnalController.js
@@ -1,5 +1,3 @@
-import UserModel from "../model/userModel";
-import jurnalSchema from "../model/jurnalModel";
 import jurnalModel from "../model/jurnalModel";
 
 export const createJurnal = async (req, res) => {
@@ -21,6 +19,85 @@ export const createJurnal = async (req, res) => {
             message: error,
             data: null
 
+        })
+    }
+}
+export const listJurnal = async (req, res) => {
+    try {
+        const data = await jurnalModel.find({})
+
+        res.status(201).json({
+            message: "List Jurnal",
+            data: data
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            data: null
+
+        })
+    }
+}
+export const updateJurnal = async (req, res) => {
+    try {
+        const id = req.params?.id
+        const request = req.body
+        if (!id) {
+            return res.status(500).json({
+                message: "Id wajib di isi",
+                data: null
+            })
+        }
+        const response = await jurnalModel.findByIdAndUpdate(id, {
+            Nama: request.Nama,
+            NPM: request.NPM,
+            Jurusan: request.Jurusan
+        })
+
+        if (!response) {
+            return res.status(500).json({
+                message: "Jurnal gagal di update",
+                data: null
+            })
+        }
+        return res.status(200).json({
+            message: "Jurnal berhasil di update",
+            data: null
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            data: null
+        })
+    }
+
+}
+export const deleteJurnal = async (req, res) => {
+    try {
+        const id = req.params.id
+
+        if (!id) {
+            return res.status(500).json({
+                message: "Id wajib di isi",
+                data: null
+            })
+        }
+        const response = await jurnalModel.findByIdAndDelete(id)
+
+        if (response) {
+            return res.status(200).json({
+                message: "Jurnal berhasil dihapus",
+                data: null
+            })
+        }
+        return res.status(404).json({
+            message: "Jurnal tidak ditemukan",
+            data: null
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error,
+            data: null
         })
     }
 }
--- route/api.js
+++ route/api.js
@@ -1,3 +1,11 @@
 import express from "express";
+import * as jurnalController from "../controllers/jurnalController.js";
 
 const api = express.Router()
+
+api.get("/jurnal", jurnalController.createJurnal)
+api.post("/jurnal", jurnalController.createJurnal)
+api.put("/jurnal/:id", jurnalController.updateJurnal)
+api.delete("/jurnal/:id", jurnalController.deleteJurnal)
+
+export default api
