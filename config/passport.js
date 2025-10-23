ef5edfe2893c433527c0f3524861cff84ef90194
CindyPatriciaK <cindypatriciakadir_2327240008@mhs.mdp.ac.id>
2025-10-23 08:04:01 +0700

    c

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import UserModel from "../models/userModel.js";
+import userModel from "../model/userModel.js";
 
 const opts = {
     jwtFromRequest : ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -9,7 +9,7 @@ const opts = {
 passport.use(
     new Strategy(opts, async(payload, done)=>{
         try{
-            const user = await UserModel.findById({
+            const user = await userModel.findById({
                 email : payload.email
             })
             
